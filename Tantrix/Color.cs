45c3757419084712b83b560fe25db9165a9dd359
Mårten Åsberg <marten.asberg@outlook.com>
2020-04-30 15:16:34 +0200

    Created Color and Tile

---
+++ Tantrix/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tantrix
+{
+	/// <summary>
+	/// Specifies constants that define the colors avalible in a Tantrix game.
+	/// </summary>
+	[Flags]
+	public enum Color : byte
+	{
+		Red    = 0b0001,
+		Green  = 0b0010,
+		Blue   = 0b0100,
+		Yellow = 0b1000
+	}
+}
---
+++ Tantrix/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tantrix
+{
+	/// <summary>
+	/// A tile.
+	/// </summary>
+	public readonly struct Tile
+	{
+		/// <summary>
+		/// The colors of the six sides in clockwise order.
+		/// </summary>
+		public ReadOnlyMemory<Color> Sides { get; }
+
+		/// <summary>
+		/// Initializes a new immutable tile.
+		/// </summary>
+		/// <param name="sides">
+		/// Provide the colors of the six sides of the new tile in clockwise
+		/// order.
+		/// </param>
+		public Tile(ReadOnlyMemory<Color> sides)
+		{
+			if (sides.Length != 6)
+			{
+				throw new ArgumentException("Tiles must have exactly 6 sides", nameof(sides));
+			}
+
+			Sides = sides;
+		}
+	}
+}

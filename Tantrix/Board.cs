8d9e2f2115e43f8dd6b2ed950117c8894a3d42e5
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-01 21:19:34 +0200

    Created Board and accompanying Position

---
+++ Tantrix/Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Tantrix
+{
+	/// <summary>
+	/// A representation of a played Tantrix.
+	/// </summary>
+	public readonly struct Board
+	{
+		/// <summary>
+		/// Creates a new empty board.
+		/// </summary>
+		public static Board Create()
+		{
+			return new Board(ImmutableDictionary.Create<Position, PlacedTile>());
+		}
+
+		/// <summary>
+		/// A collection of placed tiles at their positions.
+		/// </summary>
+		private IImmutableDictionary<Position, PlacedTile> Tiles { get; }
+
+		/// <summary>
+		/// Initializes a bord with a certain setup.
+		/// </summary>
+		/// <param name="tiles">The starting setup with tiles.</param>
+		private Board(IImmutableDictionary<Position, PlacedTile> tiles)
+		{
+			Tiles = tiles ?? throw new ArgumentNullException(nameof(tiles));
+		}
+
+		/// <summary>
+		/// Adds a tile to the board and returns the new board.
+		/// </summary>
+		/// <param name="tile">The tile to be placed.</param>
+		/// <param name="position">
+		/// The position where to place the tile.
+		/// </param>
+		/// <param name="rotation">
+		/// The number of steps the tile should be rotated [0, 6).
+		/// </param>
+		public Board PlaceTile(Tile tile, Position position, int rotation)
+		{
+			if (Tiles.ContainsKey(position))
+			{
+				throw new InvalidOperationException("Can not place a tile on top of another already place tile.");
+			}
+
+			return new Board(Tiles.Add(position, new PlacedTile(tile, rotation)));
+		}
+	}
+}
---
+++ Tantrix/Position.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Tantrix
+{
+	/// <summary>
+	/// A position on a Tantrix bord.
+	/// </summary>
+	public readonly struct Position : IEquatable<Position>
+	{
+		/// <summary>
+		/// X-coordinate, positive direction is east.
+		/// </summary>
+		public int X { get; }
+		/// <summary>
+		/// Y-coordinate, positive direction is northwest.
+		/// </summary>
+		public int Y { get; }
+		/// <summary>
+		/// Z-coordinate, positive direction is southwest.
+		/// </summary>
+		public int Z { get; }
+
+		/// <summary>
+		/// Initializes a new position at the specified coordinates.
+		/// </summary>
+		/// <param name="x">The X-coordinate.</param>
+		/// <param name="y">The Y-coordinate.</param>
+		/// <param name="z">The Z-coordinate.</param>
+		public Position(int x, int y, int z)
+		{
+			if (x + y + z != 0)
+			{
+				throw new ArgumentException("The sum of the coordinates (x + y + x) must equal 0.");
+			}
+
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		/// <inheritdoc/>
+		public override bool Equals(object obj)
+		{
+			return obj is Position position && Equals(position);
+		}
+
+		/// <inheritdoc/>
+		public bool Equals(Position other)
+		{
+			return X == other.X &&
+				   Y == other.Y &&
+				   Z == other.Z;
+		}
+
+		/// <inheritdoc/>
+		public override int GetHashCode()
+		{
+			return X * 1000000 + Y * 1000 + Z;
+		}
+
+		public static bool operator ==(Position left, Position right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Position left, Position right)
+		{
+			return !(left == right);
+		}
+
+		public static implicit operator Position((int x, int y, int z) tuple)
+		{
+			return new Position(tuple.x, tuple.y, tuple.z);
+		}
+	}
+}

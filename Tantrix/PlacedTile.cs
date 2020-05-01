ca8c48870d9999e8b44bae79df6da9d7c8375f0f
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-01 14:45:34 +0200

    Created PlacedTile for rotating tiles

---
+++ Tantrix/PlacedTile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tantrix
+{
+	/// <summary>
+	/// An immutable representation of a placed tantrix tile.
+	/// </summary>
+	readonly struct PlacedTile : ITile
+	{
+		/// <summary>
+		/// The original tile this tile is based on.
+		/// </summary>
+		private ITile BaseTile { get; }
+
+		/// <inheritdoc/>
+		public ReadOnlyMemory<Color> Sides => BaseTile.Sides;
+
+		/// <inheritdoc/>
+		public Color Colors => BaseTile.Colors;
+
+		/// <summary>
+		/// The index of the topmost side.
+		/// </summary>
+		public int Rotation { get; }
+
+		/// <summary>
+		/// Initializes an immutable tile based on <paramref name="baseTile"/>
+		/// and rotated <paramref name="rotation"/> steps.
+		/// </summary>
+		/// <param name="baseTile">The original tile.</param>
+		/// <param name="rotation">
+		/// The number of steps the tile should be rotated.
+		/// </param>
+		public PlacedTile(ITile baseTile, int rotation)
+		{
+			BaseTile = baseTile ?? throw new ArgumentNullException(nameof(baseTile));
+			Rotation = rotation;
+		}
+	}
+}
